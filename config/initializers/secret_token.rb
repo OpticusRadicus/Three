dc31e2d96b3039298b078c588bf3df29109d884e
Deltron <jamiedbuell@gmail.com>
2014-04-22 14:18:06 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Three::Application.config.secret_key_base = 'f433eaaceb9d9b234cc66ee0644ae4d784517d98a2c4737634109ce478ee81815129e4074bbc09e3091a377179f23176c25f8399dbbba8406858a8cecf1f329d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Three::Application.config.session_store :cookie_store, key: '_Three_session'
